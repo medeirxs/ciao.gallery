edb6a0ded334c042324b620044383b3af9c061fc
Matheus Medeiros <mtmedeirosgs@gmail.com>
2024-09-22 21:25:43 -0300

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <span className="text-3xl">Initial Commit</span>
+    </div>
+  );
+}
